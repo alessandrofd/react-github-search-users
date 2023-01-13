9e303390ace5754f88c284fec6674b003a84454a
Alessandro Dantas <alessandrofd@yahoo.com.br>
2023-01-13 11:23:25 -0300

    lecture-308 React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react'
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
+    <Router>
+      <Route path="/" exact>
+        <Dashboard></Dashboard>
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
       <Error />
-    </div>
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
